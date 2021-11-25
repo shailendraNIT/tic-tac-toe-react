0dc574d53c0cbc262f9e16a7a7ebdb2b962affea
shailendraNIT <63338098+shailendraNIT@users.noreply.github.com>
2021-11-25 14:02:34 +0530

    added taking turns functionality

--- src/index.js
+++ src/index.js
@@ -17,16 +17,16 @@ function Square (props){
     constructor(props){
         super(props);
         this.state={
-            squares:Array(9).fill(null)
+            squares:Array(9).fill(null),
+            xIsNext:true
         };
     }
 
     handleClick(i)
     {
         const squares=this.state.squares.slice();
-        squares[i]='X';
-        this.setState({squares:squares});
-
+        squares[i]=this.state.xIsNext?'X':'O';
+        this.setState({squares:squares,xIsNext:!this.state.xIsNext});
     }
 
 
@@ -35,7 +35,7 @@ function Square (props){
     }
   
     render() {
-      const status = 'Next player: X';
+      const status = 'Next player: ' +(this.state.xIsNext?'X':'O');
   
       return (
         <div>
